1fd3071102a9fee30700e394f08dbae58c8ded03
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2018-04-14 13:21:30 -0500

    Questions List component created

---
+++ src/app/question/questions-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Question } from './question.model';
+
+const q = new Question(
+  'How do JavaScript closures work?',
+  'How would you explain JavaScript closures ...',
+  new Date(),
+  'none'
+)
+
+@Component({
+  selector: 'app-questions-list',
+  templateUrl: './questions-list.component.html',
+  styles: [`
+    .icon {
+      width: 32px !important;
+      height: 32px !important;
+      font-size: 32px !important;
+      padding: 0 !important;
+    }
+  `
+  ]
+})
+export class QuestionsListComponent {
+  questions: Question[] = new Array(10).fill(q);
+}
