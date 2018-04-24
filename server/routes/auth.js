5bb2176f493b823b0f03e655e61855b3d4ff23fb
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2018-04-24 10:32:25 -0500

    Sign Up for new users

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,15 +29,16 @@ app.post('/signin', (req, res, next) => {
 
   if (!user) {
     debug(`User with email ${email} not found`)
-    return handleLoginFailed(res)
+    return handleLoginFailed(res, `User with email ${email} not found`)
   }
 
   if (!comparePasswords(password, user.password)) {
     debug(`Passwords do not match: ${password} !== ${user.password}`)
-    return handleLoginFailed(res)
+    return handleLoginFailed(res, 'Email and password don\'t match')
   }
 
-  const token = jwt.sign({ user }, secret, { expiresIn: 86400 })
+  const token = createToken(user)
+
   res.status(200).json({
     message: 'Login succeded',
     token,
@@ -48,10 +49,36 @@ app.post('/signin', (req, res, next) => {
   })
 })
 
-function handleLoginFailed(res) {
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  const user = {
+    _id: +new Date(),
+    firstName,
+    lastName,
+    email,
+    password
+  }
+
+  debug(`Creating new user: ${user}`)
+  users.push(user)
+
+  const token = createToken(user)
+  res.status(201).json({
+    message: 'User saved',
+    token,
+    userId: user._id,
+    firstName,
+    lastName,
+    email
+  })
+})
+
+const createToken = user => jwt.sign({ user }, secret, { expiresIn: 86400 })
+
+function handleLoginFailed(res, message) {
   return res.status(401).json({
     message: 'Login failed',
-    error: 'Email and password don\'t match'
+    error: message || 'Email and password don\'t match'
   })
 }
 
